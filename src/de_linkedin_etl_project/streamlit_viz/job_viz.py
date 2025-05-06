071298b096acb9ceb0c1aebfb359d8be3ff5b7df
nikki xie <nikki099@gmail.com>
2025-05-06 10:16:58 +0000

    split line chart by role

--- src/de_linkedin_etl_project/streamlit_viz/job_viz.py
+++ src/de_linkedin_etl_project/streamlit_viz/job_viz.py
@@ -4,6 +4,8 @@ import snowflake.connector
 import plotly_express as px
 import os
 
+st.set_page_config(layout="wide")
+
 #Snowflake connection
 def connect_to_snowflake():
     conn = snowflake.connector.connect(
@@ -29,12 +31,14 @@ def job_dates(conn):
 ## title and date range
 def title(df_dates):
     min_date = df_dates['MIN_DATE'][0]
-    max_date = df_dates['MIN_DATE'][0]
+    max_date = df_dates['MAX_DATE'][0]
     st.markdown(
         f"""
         <div style="text-align:center;margin-bottom:2rem;">
-        <div style='font-size:3rem;font-weight:700;margin-bottom:0;margin-top:0;'>Australia Data Job Trend Report</div>
-        <div style='font-size:1.3rem;font-weight:400;margin-top:0.3em;margin-bottom:0;'>Data Date Range: {min_date} - {max_date}</div>
+        <div style='font-size:2.5rem;font-weight:600;margin-bottom:0;margin-top:0;'>Australian Data Job Trends on LinkedIn</div>
+        <div style='font-size:1.3rem;font-weight:400;margin-top:0.3em;margin-bottom:0;'> {min_date} - {max_date}</div>
+        <br>
+        <div style='font-size:1.4rem;font-weight:100;margin-top:0.3em;margin-bottom:0;'> Total Jobs By Type </div>
         </div>
         """,
         unsafe_allow_html=True
@@ -79,38 +83,62 @@ def viz_job_data_summary(df_job_total):
 
 
 ## Section 2 - Daily Job Trend
+def viz_daily_job_data(conn):
+    # 查询数据
+    def query_daily_job_data(conn):
+        query = """
+            SELECT
+            DATE,
+            JOB_CATEGORY as Title,
+            SUM(TOTAL_JOBS) as Total_Jobs
+            FROM MART_TOTAL_JOBS_DAILY
+            WHERE
+            lower(title) LIKE '%data engineer%'
+            or lower(title) LIKE '%data analyst%'
+            or lower(title) LIKE '%data scientist%'
+            GROUP BY Title, DATE
+            ORDER BY Title, DATE ASC
+        """
+        df_daily_jobs = pd.read_sql(query, conn)
+        return df_daily_jobs
 
-##Get Daily jobs trend by title
-def query_daily_job_data(conn):
-    query = """
-        SELECT
-        DATE,
-        JOB_CATEGORY as Title,
-        SUM(TOTAL_JOBS) as Total_Jobs
-        FROM MART_TOTAL_JOBS_DAILY
-        WHERE
-        lower(title) LIKE '%data engineer%'
-        or lower(title) LIKE '%data analyst%'
-        or lower(title) LIKE '%data scientist%'
-        GROUP BY Title, DATE
-        ORDER BY Title, DATE ASC
-    """
-    df_daily_jobs = pd.read_sql(query, conn)
-    return df_daily_jobs
+    df_daily_jobs = query_daily_job_data(conn)
 
+    st.markdown(
+        """
+        <div style='text-align:center; font-size:1.4rem; font-weight:400; margin-top:1.4em; margin-bottom:1em;'>
+        Daily Job Trend by Role
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
 
+    job_titles = ["Data Analyst", "Data Engineer", "Data Scientist"]
+    color_map = {
+        "Data Analyst": "#1f77b4",
+        "Data Engineer": "#d62728",
+        "Data Scientist": "#aec7e8",
+    }
 
-def viz_daily_job_data(df_daily_jobs):
-    fig = px.line(df_daily_jobs, x='DATE', y='TOTAL_JOBS',
-                  color='TITLE',
-                  title='Data Job Daily Trend',
-                  color_discrete_map={
-                    "Data Analyst": "#1f77b4",
-                    "Data Scientist": "#aec7e8",
-                    "Data Engineer": "#d62728",                    }
-                  )
-    fig.update_layout(title_x=0.3) #align the title to the middle of page
-    st.plotly_chart(fig, use_container_width=True)
+    cols = st.columns(3)
+    for i, job in enumerate(job_titles):
+        df_job = df_daily_jobs[df_daily_jobs["TITLE"] == job]
+        fig = px.line(
+            df_job,
+            x="DATE",
+            y="TOTAL_JOBS",
+            title=job,
+            color_discrete_sequence=[color_map[job]]
+        )
+        fig.update_layout(
+            showlegend=False,
+            title_x=0.5,
+            margin=dict(l=10, r=10, t=40, b=10),
+            height = 300,
+            width = 420
+        )
+        cols[i].plotly_chart(fig, use_container_width=False)
+
 
 
 
@@ -120,8 +148,8 @@ def main():
     title(df_dates)
     df_job_total = query_job_total(conn)
     viz_job_data_summary(df_job_total)
-    df_daily_jobs = query_daily_job_data(conn)
-    viz_daily_job_data(df_daily_jobs)
+    viz_daily_job_data(conn)
+
 
 if __name__ == "__main__":
     main()
