755ca2185bf10dc2c7c5ba5f2a713ac171c09e9f
nikki xie <nikki099@gmail.com>
2025-05-09 15:27:01 +0000

    added more viz

--- src/de_linkedin_etl_project/streamlit_viz/job_viz1.py
+++ src/de_linkedin_etl_project/streamlit_viz/job_viz1.py
@@ -8,7 +8,7 @@ import os
 
 st.set_page_config(layout="wide")
 
-#Snowflake connection
+# Snowflake connection
 def connect_to_snowflake():
     return snowflake.connector.connect(
         user="NIKKILW2025",
@@ -19,21 +19,18 @@ def connect_to_snowflake():
         schema="linkedin_raw"
     )
 
-#Title
-##Get date ranges for data reports
 @st.cache_data
-def job_dates(conn):
+def job_dates(_conn):
     query = """
-        SELECT MIN(DATE) AS min_date, MAX(DATE) AS max_date
+        SELECT MIN(DATE) AS MIN_DATE, MAX(DATE) AS MAX_DATE
         FROM mart_total_jobs_daily
     """
-    df_dates = pd.read_sql(query, conn)
+    df_dates = pd.read_sql(query, _conn)
     return df_dates
 
-## title and date range
 def title(df_dates):
-    min_date = df_dates['MIN_DATE'][0]
-    max_date = df_dates['MAX_DATE'][0]
+    min_date = df_dates.iloc[0]['MIN_DATE']
+    max_date = df_dates.iloc[0]['MAX_DATE']
     st.markdown(
         f"""
         <div style="text-align:center; margin-bottom:2rem;">
@@ -46,7 +43,6 @@ def title(df_dates):
         </div>
         """,
         unsafe_allow_html=True)
-
     st.markdown(
         """
         <div style='text-align:center; font-size:1.65rem; font-weight:600; margin-bottom:0.7em; margin-top:0.7em;'>
@@ -57,12 +53,8 @@ def title(df_dates):
         unsafe_allow_html=True
     )
 
-## Section 1 - Job Data Summary
-
-##Get total jobs by title
-
 @st.cache_data
-def query_job_total(conn):
+def query_job_total(_conn):
     query = """
         SELECT JOB_CATEGORY as Title,
         COUNT(DISTINCT ID) as Total_Jobs
@@ -74,12 +66,9 @@ def query_job_total(conn):
         GROUP BY JOB_CATEGORY
         ORDER BY Title ASC
     """
-    df_job_total = pd.read_sql(query, conn)
+    df_job_total = pd.read_sql(query, _conn)
     return df_job_total
 
-
-## Dashboard viz - total job summary
-@st.cache_data
 def viz_job_data_summary(df_job_total):
     col1, col2, col3 = st.columns(3)
     with col1:
@@ -95,32 +84,23 @@ def viz_job_data_summary(df_job_total):
             f"<div style='text-align:center;'><span style='font-size:50px;font-weight:bold;color:#0099ff;'>{int(df_job_total.iloc[2,1])}</span><br><b>Data Scientist</b></div>",
             unsafe_allow_html=True)
 
-
-
-## Section 2 - WoW Job Trend
 @st.cache_data
-def viz_wow_trend(conn):
-    # today = datetime.now()
-    # current_week = today - timedelta(days=today.weekday())
-    # current_week = pd.to_datetime(current_week).date()
-    # print(f"this week starting on {current_week}")
-
-    #wow data query#
+def query_wow_trend(_conn):
     query_wow = """
             SELECT
             WEEK_START,
             JOB_CATEGORY,
-            THIS_WEEK_JOBS as this_week,
-            LAST_WEEK_JOBS as LAST_week,
+            THIS_WEEK_JOBS as THIS_WEEK,
+            LAST_WEEK_JOBS as LAST_WEEK,
             WOW_DIFF_PCT
             FROM MART_TOTAL_JOBS_WOW_TREND
             ORDER BY WEEK_START DESC
             LIMIT 3
         """
-    df_wow_trend = pd.read_sql(query_wow, conn)
+    df_wow_trend = pd.read_sql(query_wow, _conn)
+    return df_wow_trend
 
-
-    #create viz
+def viz_wow_trend(df_wow_trend):
     col1, col2, col3 = st.columns(3)
     for idx, role in enumerate(df_wow_trend['JOB_CATEGORY']):
         wow_pct = f"{df_wow_trend.loc[idx, 'WOW_DIFF_PCT'] * 100:.0f}%"
@@ -139,45 +119,37 @@ def viz_wow_trend(conn):
                 unsafe_allow_html=True
             )
 
-
-
-## Section 3 - Daily Job Trend
 @st.cache_data
-def viz_daily_job_data(conn):
-    # data query
-    def query_daily_job_data(conn):
-        query = """
-            SELECT
-            DATE,
-            JOB_CATEGORY as Title,
-            SUM(TOTAL_JOBS) as Total_Jobs
-            FROM MART_TOTAL_JOBS_DAILY
-            WHERE
-            lower(title) LIKE '%data engineer%'
-            or lower(title) LIKE '%data analyst%'
-            or lower(title) LIKE '%data scientist%'
-            GROUP BY Title, DATE
-            ORDER BY Title, DATE ASC
-        """
-        df_daily_jobs = pd.read_sql(query, conn)
-        return df_daily_jobs
+def query_daily_job_data(_conn):
+    query = """
+        SELECT
+        DATE,
+        JOB_CATEGORY as TITLE,
+        SUM(TOTAL_JOBS) as TOTAL_JOBS
+        FROM MART_TOTAL_JOBS_DAILY
+        WHERE
+        lower(title) LIKE '%data engineer%'
+        or lower(title) LIKE '%data analyst%'
+        or lower(title) LIKE '%data scientist%'
+        GROUP BY TITLE, DATE
+        ORDER BY TITLE, DATE ASC
+    """
+    df_daily_jobs = pd.read_sql(query, _conn)
+    return df_daily_jobs
 
-    df_daily_jobs = query_daily_job_data(conn)
-
+def viz_daily_job_data(df_daily_jobs):
     st.markdown("""
     <div style='text-align:center; font-size:1.65rem; font-weight:600; margin-bottom:0.7em; margin-top:0.7em;'>
         Daily Job Trend
         <hr style="border:1px solid #bbb; width:70%; margin:auto;">
     </div>
     """, unsafe_allow_html=True)
-
     job_titles = ["Data Analyst", "Data Engineer", "Data Scientist"]
     color_map = {
         "Data Analyst": "#1f77b4",
         "Data Engineer": "#d62728",
         "Data Scientist": "#aec7e8",
     }
-
     cols = st.columns(3)
     for i, job in enumerate(job_titles):
         df_job = df_daily_jobs[df_daily_jobs["TITLE"] == job]
@@ -192,41 +164,32 @@ def viz_daily_job_data(conn):
             showlegend=False,
             title_x=0.5,
             margin=dict(l=10, r=10, t=40, b=10),
-            height = 300,
-            width = 420
+            height=300,
+            width=420
         )
         cols[i].plotly_chart(fig, use_container_width=False)
 
-
-
-## Section 4 - Job Salary
 @st.cache_data
-def viz_job_salary(conn):
-    #salary range by role query
+def query_salary_data(_conn):
+    query = """
+        SELECT
+        JOB_TITLE,
+        AVG(SALARY_RANGE_LOW) as SALARY_LOW,
+        AVG(SALARY_RANGE_HIGH) as SALARY_HIGH
+        FROM MART_JOB_SALARY
+        GROUP BY JOB_TITLE
+        ORDER BY JOB_TITLE ASC
+    """
+    df_salary = pd.read_sql(query, _conn)
+    return df_salary
 
-    def query_salary_data(conn):
-        query = """
-            SELECT
-            JOB_TITLE,
-            AVG(SALARY_RANGE_LOW) as salary_low,
-            AVG(SALARY_RANGE_HIGH) as salary_high
-            FROM MART_JOB_SALARY
-            GROUP BY JOB_TITLE
-            ORDER BY JOB_TITLE ASC
-        """
-        df_salary = pd.read_sql(query, conn)
-        return df_salary
-
-    df_salary = query_salary_data(conn)
-
-
+def viz_job_salary(df_salary):
     st.markdown("""
     <div style='text-align:center; font-size:1.65rem; font-weight:600; margin-bottom:0.7em; margin-top:0.7em;'>
         Salary by Job Role
         <hr style="border:1px solid #bbb; width:70%; margin:auto;">
     </div>
     """, unsafe_allow_html=True)
-
     col1, col2, col3 = st.columns(3)
     for idx, row in df_salary.iterrows():
         role = row['JOB_TITLE']
@@ -234,11 +197,9 @@ def viz_job_salary(conn):
         max_salary = row['SALARY_HIGH']
         lower_bound = min_salary - (max_salary - min_salary)/2
         upper_bound = max_salary + (max_salary - min_salary)/2
-
         bar1 = min_salary - lower_bound
         bar2 = max_salary - min_salary
         bar3 = upper_bound - max_salary
-
         fig = go.Figure()
         fig.add_trace(go.Bar(
             x=[bar1],
@@ -269,7 +230,6 @@ def viz_job_salary(conn):
             hoverinfo='skip',
             showlegend=False,
         ))
-
         fig.update_layout(
             barmode='relative',
             height=120,
@@ -283,8 +243,6 @@ def viz_job_salary(conn):
             plot_bgcolor='white',
             paper_bgcolor='white'
         )
-
-        # Bar notation
         with [col1, col2, col3][idx]:
             st.markdown(
                 f"<div style='text-align:center; font-weight:400; margin-bottom:0.5rem'>{role}</div>", unsafe_allow_html=True
@@ -294,74 +252,152 @@ def viz_job_salary(conn):
             )
             st.plotly_chart(fig, use_container_width=True)
 
-
-## Section 5 - Top Job Skills
 @st.cache_data
-def viz_top_job_skills(conn):
-    #job skills by role query
+def query_top_skills_data(_conn):
+    query = """
+        SELECT
+        *
+        FROM MART_TOP_JOB_SKILLS
+        ORDER BY "Job_Title"
+    """
+    df_top_skills = pd.read_sql(query, _conn)
+    return df_top_skills
+
+def viz_top_job_skills(df_top_skills):
     st.markdown("""
     <div style='text-align:center; font-size:1.65rem; font-weight:600; margin-bottom:0.7em; margin-top:0.7em;'>
         Top Skills Required by Job Role
         <hr style="border:1px solid #bbb; width:70%; margin:auto;">
     </div>
     """, unsafe_allow_html=True)
-
-    def query_top_skills_data(conn):
-        query = """
-            SELECT
-            *
-            FROM MART_TOP_JOB_SKILLS
-            ORDER BY "Job_Title"
-        """
-        df_top_skills = pd.read_sql(query, conn)
-        return df_top_skills
-
-    df_top_skills = query_top_skills_data(conn)
-
     st.dataframe(df_top_skills, use_container_width=True)
 
-
-
-## Section 6 - Top Job by States and Cities
 @st.cache_data
-def query_job_by_region_org(conn):
+def query_job_by_region_org(_conn):
+    st.markdown("""
+    <div style='text-align:center; font-size:1.65rem; font-weight:600; margin-bottom:0.7em; margin-top:0.7em;'>
+        Job Stats by State, City & Organization
+        <hr style="border:1px solid #bbb; width:70%; margin:auto;">
+    </div>
+    """, unsafe_allow_html=True)
+
     state_query = """
-        SELECT *
-        FROM MART_TOTAL_JOBS_BY_STATE
-        PIVOT(
-            SUM(TOTAL_JOBS) FOR JOB_CATEGORY IN ('Data Analyst', 'Data Engineer', 'Data Scientist')
-        )
+        SELECT * FROM MART_TOTAL_JOBS_BY_STATE
         ORDER BY
             CASE WHEN STATE = 'UNKNOWN' THEN 1 ELSE 0 END,
             STATE
     """
-    df_state_jobs = pd.read_sql(state_query, conn)
+    df_state_jobs = pd.read_sql(state_query, _conn)
 
-    city_query_base = """
-        SELECT * FROM MART_TOTAL_JOBS_BY_STATE
-        ORDER BY STATE, JOB_CATEGORY
-        """
-    df_city_jobs = pd.read_sql(city_query_base, conn)
+    city_query = """
+        SELECT * FROM MART_TOTAL_JOBS_BY_CITY
+        ORDER BY CITY, JOB_CATEGORY
+    """
+    df_city_jobs = pd.read_sql(city_query, _conn)
 
-
-    org_query_base = """
-        SELECT * FROM MART_TOTAL_JOBS_BY_ORG
+    org_query = """
+        SELECT JOB_CATEGORY, ORGANIZATION, TOTAL_JOBS FROM MART_TOTAL_JOBS_BY_ORG
         ORDER BY ORGANIZATION, JOB_CATEGORY
     """
-    df_org_jobs = pd.read_sql(org_query_base, conn)
-
+    df_org_jobs = pd.read_sql(org_query, _conn)
     return df_state_jobs, df_city_jobs, df_org_jobs
 
-   #viz
 def viz_job_by_region_org(dfs):
     df_state_jobs, df_city_jobs, df_org_jobs = dfs
+
+    #Job_category for 3 all dfs (dediplicated, sorted)
+    all_job_categories = sorted(
+        set(df_state_jobs['JOB_CATEGORY'].dropna().unique())
+        | set(df_city_jobs['JOB_CATEGORY'].dropna().unique())
+        | set(df_org_jobs['JOB_CATEGORY'].dropna().unique())
+    )
+
+    #Top Job Role Selector
+    select_job_category=st.selectbox("Filter by Role", ['All'] + all_job_categories)
+
+    #Filter function
+    def filter_by_role(df):
+        if select_job_category != 'All':
+            return df[df['JOB_CATEGORY']==select_job_category]
+        return df
+
+    filtered_state =  filter_by_role(df_state_jobs)
+    filtered_state = filtered_state.sort_values(by='TOTAL_JOBS', ascending=False)
+    filtered_city =  filter_by_role(df_city_jobs)
+    filtered_city = filtered_city.sort_values(by='TOTAL_JOBS', ascending=False)
+    filtered_org =  filter_by_role(df_org_jobs)
+    filtered_org = filtered_org.sort_values(by='TOTAL_JOBS', ascending=False)
+
+    #Jobs by Role Table Viz
     col1, col2, col3 = st.columns(3)
     with col1:
-        st.dataframe(df_state_jobs)
+        st.dataframe(filtered_state)
     with col2:
-        st.dataframe(df_city_jobs)
+        st.dataframe(filtered_city)
     with col3:
-        st.dataframe(df_org_jobs)
+        st.dataframe(filtered_org)
+
+@st.cache_data
+def query_job_details(_conn):
+    st.markdown("""
+    <div style='text-align:center; font-size:1.65rem; font-weight:600; margin-bottom:0.7em; margin-top:0.7em;'>
+        Job Information
+        <hr style="border:1px solid #bbb; width:70%; margin:auto;">
+    </div>
+    """, unsafe_allow_html=True)
+
+
+    job_info_query = """
+        SELECT DISTINCT JOB_CATEGORY AS Job_Role,
+        TITLE, JOB_DATE AS Post_Date,
+        EMPLOYMENT_TYPE, SENIORITY, CITY, STATE, ORGANIZATION,
+        URL AS LinkedIn_Post_Link,
+        LINKEDIN_ORG_URL,
+        LINKEDIN_ORG_INDUSTRY,
+        REMOTE_DERIVED AS IS_REMOTE,
+        LINKEDIN_ORG_RECRUITMENT_AGENCY_DERIVED AS Job_By_Agency,
+        DIRECTAPPLY
+        FROM INT_LINKEDIN_DATA
+        ORDER BY JOB_DATE DESC, JOB_CATEGORY ASC
+    """
+    df_job_details = pd.read_sql(job_info_query, _conn)
+    return df_job_details
+
+
+def viz_job_details(df_job_details):
+    job_category = sorted(
+        set(df_job_details['JOB_ROLE'].dropna().unique())
+    )
+    select_job_category = st.selectbox(
+        "Filter by Role", ['All'] + job_category, key="filter_by_role_jobdetails"
+    )
+
+    def filter_by_role(df):
+        if select_job_category != 'All':
+            return df[df['JOB_ROLE'] == select_job_category]
+        return df
+
+    filtered_job_table = filter_by_role(df_job_details)
+    # 新建展示用列（名字不能重复）
+    if not filtered_job_table.empty:
+        show_df = filtered_job_table.copy()
+
+        #get the position - 10
+        columns = show_df.columns.tolist()
+        idx= columns.index("LINKEDIN_POST_LINK")
+        show_df.insert(idx, "Job Link", show_df["LINKEDIN_POST_LINK"])
+        show_df = show_df.drop(columns=["LINKEDIN_POST_LINK"])
+
+        st.data_editor(
+            show_df,
+            column_config={
+                "Job Link": st.column_config.LinkColumn("Job Link")
+            },
+            hide_index=True,
+            height=600
+        )
+    else:
+        st.info('No data for the selected role.')
 
 
 
@@ -372,12 +408,18 @@ def main():
     title(df_dates)
     df_job_total = query_job_total(conn)
     viz_job_data_summary(df_job_total)
-    viz_wow_trend(conn)
-    viz_daily_job_data(conn)
-    viz_job_salary(conn)
-    viz_top_job_skills(conn)
-    dfs = query_job_by_region_org(conn)
-    viz_job_by_region_org(dfs)
+    df_wow_trend = query_wow_trend(conn)
+    viz_wow_trend(df_wow_trend)
+    df_daily_jobs = query_daily_job_data(conn)
+    viz_daily_job_data(df_daily_jobs)
+    df_salary = query_salary_data(conn)
+    viz_job_salary(df_salary)
+    df_top_skills = query_top_skills_data(conn)
+    viz_top_job_skills(df_top_skills)
+    dfs_region_org = query_job_by_region_org(conn)
+    viz_job_by_region_org(dfs_region_org)
+    df_job_details = query_job_details(conn)
+    viz_job_details(df_job_details)
 
 if __name__ == "__main__":
     main()
